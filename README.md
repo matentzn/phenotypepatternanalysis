8f387766c7d169ffcc12d226172007fd2c2fee37
matentzn <matentzn@spotgroup02-ml.windows.ebi.ac.uk>
2018-02-05 14:53:29 +0000

    Major overhaul. Instead of docker main execution now script based

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# phenotypepatternanalysis
+# Pattern analysis
 This repository contains the ongoing work of extracting and documenting phenotype related patterns across relevant phenotype ontologies
--- redeploy.sh
+++ redeploy.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+# Settings
+repo=/ws/phenotypepatternanalysis
+corpus=corpus_big.zip
 data=/data/dockerdata
-results=/ws/phenotypepatternanalysis/out
+
+# Pipeline
+results=$repo/out
+corpus_archive=$repo/in/$corpus
 
 rm -r $results
 mkdir $results
-docker build --no-cache -t ppa .
-docker run -v $data:/data  -p 4000:80 ppa
-cp $data/*.txt $results
-cp $data/*.md $results
-cp $data/*.csv $results
-cd /ws/phenotypepatternanalysis
-Rscript -e "library(knitr); knit('pattern_analysis.Rmd')"
 
-zip -r $results/ontologies.zip $data/*.owl
+rm -r $data
+mkdir $data
+mkdir $data/out
+
+cp $corpus_archive $data/corpus.zip
+cd $data
+unzip corpus.zip
+docker pull matentzn/ebi-patternanalysis-pipeline
+docker pull matentzn/r-knitr
+docker run -v $data:/data  -p 4000:80 matentzn/ebi-patternanalysis-pipeline
+docker run -v $data/out:/data  -p 4000:80 matentzn/r-knitr
+cp $data/out/*.md $results
+cp $data/out/*.csv $results
+zip -r $results/ontologies.zip $data/out/*.owl
